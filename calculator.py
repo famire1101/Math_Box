e2594584ce21619b8fc8a45c71f5cad613d9ad5c
kimhyojin <grinelphie@gmail.com>
2024-01-09 18:19:10 +0900

    Add squre function

--- calculator.py
+++ calculator.py
@@ -1,9 +1,15 @@
 #기본 계산기
 def add(a, b):
   return a+b
+
 def subtract(a, b):
   return a-b
+
 def multifly(a, b):
   return a*b
+
 def divide_free(a, b):
   return a/b
+
+def squre(a):
+  return a*a
